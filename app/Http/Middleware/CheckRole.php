9948052c3ba47f818d3b4311639ebc9bc69d1b0c
Zubayer Hossain <zubayerh4@gmail.com>
2024-06-06 20:10:53 +0600

    Created middleware CheckRole

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  string[]  ...$roles
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (!in_array($request->user()->role, $roles)) {
+            abort(403, 'Unauthorized');
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // Register CheckRole middleware
+        $middleware->alias('role', \App\Http\Middleware\CheckRole::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
