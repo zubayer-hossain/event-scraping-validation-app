27baf18d0661b1bd9e5b1e76f039fda565cce063
Zubayer Hossain <zubayerh4@gmail.com>
2024-06-10 23:07:07 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::get('events', [EventController::class, 'index'])->name('events.index');
 
     Route::middleware('role:author')->group(function () {
-        Route::get('events/list', [EventController::class, 'list'])->name('events.list');
         Route::get('events/create', [EventController::class, 'create'])->name('events.create');
         Route::post('events', [EventController::class, 'store'])->name('events.store');
         Route::get('events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
